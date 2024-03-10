65fa47768695fd3b35e7d17f87c053fbb474e97f
lvarezhkal <kkakxaa@gmail.com>
2024-03-10 20:11:08 +0300

    allDone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            checkLoginAndPassword("zarplata", "Pupa1", "Lupa2");
 
+        }catch (WrongLoginException | WrongPasswordException e){
+            e.printStackTrace();
+        }
+    }
+    public static void checkLoginAndPassword(String login, String password, String confirmPassword) {
+
+        if (login.length() > 20){
+            throw new WrongLoginException("Слишком длинный логин.");
+        }
+        if (!login.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongLoginException("Недопустимые символы.");
+        }
+        if (password.length() > 20){
+            throw new WrongLoginException("Слишком длинный пароль.");
+        }
+        if (!password.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongLoginException("Недопустимые символы.");
+        }
+        if (!password.equals(confirmPassword)){
+            throw new WrongPasswordException("Несовпадение паролей.");
+        }
     }
 }
